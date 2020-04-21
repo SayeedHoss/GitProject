c00e4ed035716e756c50c10480815cb696eed7d7
selenium87 <61388947+selenium87@users.noreply.github.com>
2020-04-20 21:33:09 -0700

    Update sukrat.java

--- src/main/java/Sayeed/sukrat.java
+++ src/main/java/Sayeed/sukrat.java
@@ -1,1 +1,34 @@
+package Sayeed;
 
+public class sukrat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		System.out.println("sukrat class");
+		
+		System.out.println("sayeed");
+		
+		
+
+		System.out.println("sukrat class");
+		
+		System.out.println("sayeed");
+		System.out.println("sukrat class");
+		
+		System.out.println("sayeed");
+		
+System.out.println("i got the stuff back");
+		
+		System.out.println("sayeed");
+		System.out.println("crazy people");
+	}
+	public void crazypeople() {
+	
+		System.out.println("super crazy");
+	
+	
+}
+}
