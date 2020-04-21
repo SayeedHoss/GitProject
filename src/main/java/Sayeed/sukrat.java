7f846db0d0239743174430ba42b0389cc3a51c99
husky <gpixlexl@gmail.com>
2020-04-20 22:43:17 -0700

    first class added

---
+++ src/main/java/Sayeed/sukrat.java
@@ -0,0 +1,13 @@
+package Sayeed;
+
+public class sukrat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		System.out.println("sukrat class");
+	}
+
+}
